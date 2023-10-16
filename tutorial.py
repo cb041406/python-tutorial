a70f34a821e84e55da129f6e60a8168d5eca6f1d
cb041406 <cb041406@wgcloud.org>
2023-10-16 11:02:58 -0500

    Added steps to readme, added name collection & output

--- tutorial.py
+++ tutorial.py
@@ -3,3 +3,8 @@ print("Hello World")
 # added second message
 
 print("Hello Caroline")
+
+#collect new user's name
+name = input ("Enter you name: ")
+#output welcome message
+print("Welcome" +name)
